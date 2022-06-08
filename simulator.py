ac653c22b90853e8ef3da4a33c2679363142abb3
Sergey Smalkov <36485527+SergeySmalkov@users.noreply.github.com>
2022-06-08 19:27:54 +0300

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  7 13:38:08 2022
+
+@author: cheytakker
+"""
 
 import pandas as pd
 import numpy as np
@@ -8,91 +15,140 @@ class Simulator:
     class OB:
         def __init__(self, current_time, bids, asks):
             self.current_time = current_time
-            self.bids = bids
-            self.asks = asks
-            self.current_mid = (self.bids[0][1] + self.asks[0][1]) / 2
-            self.best_bid = self.bids[0][1]
-            self.best_ask = self.asks[0][1]
+            self.bids = pd.DataFrame(bids, columns = ["P", "Q", "i"])
+            self.asks = pd.DataFrame(asks, columns = ["P", "Q", "i"])
+            self.best_bid = {"bb_index" : len(self.bids) - 1, "bb_price" : 0.0, 
+                             "bb_i" : self.bids["i"][len(self.bids) - 1]}
+            self.best_ask = {"ba_index" : len(self.asks) - 1, "ba_price" : 0.0, 
+                             "ba_i" : self.asks["i"][len(self.asks) - 1]}
+            self.first_lvls = {"bid_i" : 0, "ask_i" : 0, "bid_index" : 0, "ask_index": 0} # i - relatively to mid, index - in dataframe
+            self.mid = 0.0
+        
+            self.refresh_lvls()
+            
             
         def process_market_order(self, market_order):
-            market_order = list(market_order)
-            market_order.append(0.0)
-            market_order.append(0.0)
-            if (market_order[1] == 1.0):
-                while (market_order[2] != 0.0):
-                    if (len(self.asks) == 0.0): break
-                    if (self.asks[0][1] >= market_order[2]):
+            market_order = market_order.to_dict()
+            market_order["Unexecuted_Q"] = market_order.pop("Q")
+            market_order["Executed_Q"] = 0.0
+            market_order["VWAP"] = 0.0 
+            
+            if (market_order["d"] == 1.0):
+                lvl = self.best_ask["ba_index"]
+                while (market_order["Unexecuted_Q"] != 0.0):
+                    if (len(self.asks) == lvl): break
+                    if (self.asks.loc[lvl, "Q"] == 0.0): 
+                        lvl += 1 
+                        continue
+                    if (self.asks.loc[lvl, "Q"] >= market_order["Unexecuted_Q"]):
 
-                        p1 = market_order[4]
-                        q1 = market_order[3]
-                        p2 = self.asks[0][0]
-                        q2 = market_order[2]
-                        self.asks[0][1] -= q2
-                        market_order[3] += q2                                   #Executed amount
-                        market_order[4] = (p1 * q1 + p2 * q2) / (q1 + q2)       #VWAP
-                        market_order[2] = 0.0                                   #Unexecuted amount
+                        p1 = market_order["VWAP"]
+                        q1 = market_order["Executed_Q"]
+                        p2 = self.asks.loc[lvl, "P"]
+                        q2 = market_order["Unexecuted_Q"]
+                        self.asks.loc[lvl, "Q"] -= q2
+                        market_order["Executed_Q"] += q2                                   
+                        market_order["VWAP"] = (p1 * q1 + p2 * q2) / (q1 + q2)       
+                        market_order["Unexecuted_Q"] = 0.0 
+                                 
+                    elif (self.asks.Q[lvl] < market_order["Unexecuted_Q"]):
+
+                        p1 = market_order["VWAP"]
+                        q1 = market_order["Executed_Q"]
+                        p2 = self.asks.loc[lvl, "P"]
+                        q2 = self.asks.loc[lvl, "Q"]
+                        self.asks.loc[lvl, "Q"] = 0.0
+                        market_order["Executed_Q"] += q2                                   
+                        market_order["VWAP"] = (p1 * q1 + p2 * q2) / (q1 + q2)       
+                        market_order["Unexecuted_Q"] -= q2                                   
+                        lvl += 1
                         
-                    elif (self.asks[0][1] < market_order[2]):
+            elif (market_order["d"] == -1.0):
+                lvl = self.best_bid["bb_index"]
+                while (market_order["Unexecuted_Q"] != 0.0  ):
+                    if (len(self.bids) == lvl): break
+                    if (self.bids.Q[lvl] == 0.0):
+                        lvl += 1
+                        continue
+                    if (self.bids.loc[lvl, "Q"] >= market_order["Unexecuted_Q"]):
 
-                        p1 = market_order[4]
-                        q1 = market_order[3]
-                        p2 = self.asks[0][0]
-                        q2 = self.asks[0][1]
-                        self.asks[0][1] = 0.0
-                        market_order[3] += q2                                   #Executed amount
-                        market_order[4] = (p1 * q1 + p2 * q2) / (q1 + q2)       #VWAP
-                        market_order[2] -= q2                                   #Unexecuted amount
-                        self.asks.pop(0)
-                        
-            elif (market_order[1] == -1.0):
-                while (market_order[2] != 0.0  ):
-                    if (len(self.bids) == 0): break
-                    if (self.bids[0][1] >= market_order[2]):
- 
-                        p1 = market_order[4]
-                        q1 = market_order[3]
-                        p2 = self.bids[0][0]
-                        q2 = market_order[2]
-                        self.bids[0][1] -= q2
-                        market_order[3] += q2                                   #Executed amount
-                        market_order[4] = (p1 * q1 + p2 * q2) / (q1 + q2)       #VWAP
-                        market_order[2] = 0.0                                   #Unexecuted amount
-                    elif (self.bids[0][1] < market_order[2]):
+                        p1 = market_order["VWAP"]
+                        q1 = market_order["Executed_Q"]
+                        p2 = self.bids.loc[lvl, "P"]
+                        q2 = market_order["Unexecuted_Q"]
+                        self.bids.loc[lvl, "Q"]-= q2
+                        market_order["Executed_Q"] += q2                                   
+                        market_order["VWAP"] = (p1 * q1 + p2 * q2) / (q1 + q2)      
+                        market_order["Unexecuted_Q"] = 0.0                                   
+                    elif (self.bids.loc[lvl, "Q"] < market_order["Unexecuted_Q"]):
 
-                        p1 = market_order[4]
-                        q1 = market_order[3]
-                        p2 = self.bids[0][0]
-                        q2 = self.bids[0][1]
-                        self.asks[0][1] = 0.0
-                        market_order[3] += q2                                   #Executed amount
-                        market_order[4] = (p1 * q1 + p2 * q2) / (q1 + q2)       #VWAP
-                        market_order[2] -= q2                                   #Unexecuted amount
-                        self.bids.pop(0)
-            if(len(self.bids)): self.best_bid = self.bids[0][1] 
-            else: self.best_bid = 0.0
-            if(len(self.asks)): self.best_ask = self.asks[0][1] 
-            else: self.best_ask = 0.0
-
+                        p1 = market_order["VWAP"]
+                        q1 = market_order["Executed_Q"]
+                        p2 = self.bids.loc[lvl, "P"]
+                        q2 = self.bids.loc[lvl, "Q"]
+                        self.bids.loc[lvl, "Q"] = 0.0
+                        market_order["Executed_Q"] += q2                                   
+                        market_order["VWAP"] = (p1 * q1 + p2 * q2) / (q1 + q2)       
+                        market_order["Unexecuted_Q"] -= q2                                   
+                        lvl += 1
+            
+            self.refresh_lvls()
             return market_order
         
         def process_cancelation(self, cncl_order):
-            if (cncl_order[1] == -1):
-                 
-                index = np.where([i == cncl_order[3] for i in [row[2] for row in self.asks]])[0]
-                if(len(index)) :
-                    self.asks[index[0]][1] = max(0.0, self.asks[index[0]][1] - cncl_order[2])
+            if (cncl_order["d"] == -1):
+                index = self.first_lvls["ask_index"] + cncl_order.i
+                if(index < len(self.asks)) :
+                    self.asks.loc[index, "Q"] = max(0.0, self.asks.loc[index, "Q"] - cncl_order.Q)
+                    if(cncl_order.i == 0): self.refresh_lvls()     #if ToB was changed - refresh the indexes of OB
                 else: return
                     
                     
-            elif(cncl_order[1] == 1):
+            elif(cncl_order["d"] == 1):
                 
-                index = np.where([i == cncl_order[3] for i in [row[2] for row in self.bids]])[0]
-                if(len(index)) :
-                    self.bids[index[0]][1] = max(0.0, self.bids[index[0]][1] - cncl_order[2])
+                index = self.first_lvls["bid_index"] + cncl_order.i
+                if(index < len(self.bids)) :
+                    self.bids.loc[index, "Q"] = max(0.0, self.bids.loc[index, "Q"] - cncl_order.Q)
+                    if(cncl_order.i == 0): self.refresh_lvls()     #if ToB was changed - refresh the indexes of OB
                 else: return
+                
+        def refresh_lvls(self):
+            if(len(self.bids[self.bids["Q"] != 0.0]) == 0): print("Attention - Empty Order Book")
+            if(len(self.asks[self.asks["Q"] != 0.0]) == 0): print("Attention - Empty Order Book")
+            self.best_bid["bb_index"] = self.bids[self.bids["Q"] != 0.0]["P"].idxmax()
+            self.best_ask["ba_index"] = self.asks[self.asks["Q"] != 0.0]["P"].idxmin()
+            self.best_bid["bb_i"] = self.bids["i"][self.best_bid["bb_index"]]
+            self.best_ask["ba_i"] = self.asks["i"][self.best_ask["ba_index"]]
+            self.best_bid["bb_price"] = self.bids["P"][self.best_bid["bb_index"]]
+            self.best_ask["ba_price"] = self.asks["P"][self.best_ask["ba_index"]]
+            self.mid = round((self.best_bid["bb_price"] + self.best_ask["ba_price"]) / 2, 5)
+
+            mid_ind = (self.best_ask["ba_i"] - self.best_bid["bb_i"]) / 2
+            self.first_lvls["ask_i"] = int(np.ceil(mid_ind))
+            self.first_lvls["bid_i"] = int(-np.floor(mid_ind))
+            self.first_lvls["ask_index"] = int(self.asks.index[self.asks['i'] == self.first_lvls["ask_i"]][0])
+            self.first_lvls["bid_index"] = int(self.bids.index[self.bids['i'] == self.first_lvls["bid_i"]][0])
+            
                 
         def process_limit_order(self, limit_order):
-            #update index in market order and cancelation to proceed
+            
+            if (limit_order["d"] == -1):
+                index = self.first_lvls["ask_index"] + limit_order.i
+
+                if(index < len(self.asks)) :
+                    self.asks.loc[index, "Q"] = self.asks.loc[index, "Q"] + limit_order.Q
+                    if(index < self.best_ask["ba_index"]): self.refresh_lvls()     #if ToB was changed - refresh the indexes of OB
+                else: return
+                
+            elif (limit_order["d"] == 1):
+                index = self.first_lvls["bid_index"] + limit_order.i
+
+                if(index < len(self.bids)) :
+                    self.bids.loc[index, "Q"] = self.bids.loc[index, "Q"] + limit_order.Q
+                    if(index < self.best_bid["bb_index"]): self.refresh_lvls()     #if ToB was changed - refresh the indexes of OB
+                else: return
+                
+                
                 
             
     def __init__(self, time, lambda_0_market, a, var_e, q_0, var_q, b, start_mid, depth, step, density, lambda_0_limit, lambda_0_cncl):
@@ -103,6 +159,7 @@ class Simulator:
         self.q_0 = q_0           # average deal quantity
         self.var_q = var_q       # variance of corresponding normal for deal quantity 
         self.b = b               # coefficient for direction correlation
+        self.shelf = 3
         
         self.start_mid = start_mid # start mid
         self.depth = depth         # depth of the order book
@@ -114,23 +171,30 @@ class Simulator:
         
         self.OB_0 = self.start_OB()
         
-        self.market_orders_report = []
-        self.market_orders = self.simulate_market_orders()
-        self.limit_orders = self.simulate_limit_orders(self.lambda_0_limit)
-        self.cancelations = self.simulate_limit_orders(self.lambda_0_cncl)
+        self.market_orders_report = pd.DataFrame(columns = ["t", "d", "Unexecuted_Q", "Executed_Q", "VWAP"])
+        self.market_orders = pd.DataFrame(self.simulate_market_orders(), columns = ["t", "d", "Q"])
+        self.limit_orders = pd.DataFrame(self.simulate_limit_orders(self.lambda_0_limit), columns = ["t", "d", "Q", "i"]) 
+        self.cancelations = pd.DataFrame(self.simulate_limit_orders(self.lambda_0_cncl), columns = ["t", "d", "Q", "i"])
         
+        self.order_books = self.matching()
         
     def start_OB(self):
-        lvls_bids = np.random.choice(self.depth, round(self.depth * self.density) , replace=False)
+        lvls_bids = np.random.choice(range(1, self.depth + 1), round(self.depth * self.density) , replace=False)
         lvls_bids.sort()
-        lvls_asks = np.random.choice(self.depth, round(self.depth * self.density) , replace=False)
+        lvls_asks = np.random.choice(range(1, self.depth + 1), round(self.depth * self.density) , replace=False)
         lvls_asks.sort()
-        bids = [[self.start_mid - (1 + i) * self.step, 
-                      round(np.random.lognormal(np.log(self.q_0) - self.var_q / 2, math.sqrt(self.var_q)), 4), i] 
-                     for i in lvls_bids]
-        asks = [[self.start_mid + (1 + i) * self.step, 
-                      round(np.random.lognormal(np.log(self.q_0) - self.var_q / 2, math.sqrt(self.var_q)),4), i]
-                     for i in lvls_asks]
+
+        bids = [[self.start_mid - i  * self.step, 
+                      round(np.random.lognormal(np.log(self.q_0) - self.var_q / 2, math.sqrt(self.var_q)), 4) 
+                      if i in lvls_bids else 0.0, i] 
+                     for i in range(-self.depth * self.shelf , self.depth * self.shelf + 1)]
+    
+        asks = [[self.start_mid + i  * self.step, 
+                      round(np.random.lognormal(np.log(self.q_0) - self.var_q / 2, math.sqrt(self.var_q)),4) 
+                      if i in lvls_asks else 0.0, i]
+                     for i in range(-self.depth * self.shelf , self.depth * self.shelf + 1)]
+    
+    
         ob = self.OB(0, bids, asks)
         return ob
      
@@ -212,26 +276,44 @@ class Simulator:
         len_m = len(self.market_orders)
         len_c = len(self.cancelations)
         len_l = len(self.limit_orders)
-        self.market_orders.append((self.time + 1, -1.0, 1))   #breakpoint row,  will be deleted
-        self.cancelations.append((self.time + 1, -1.0, 1, 0)) #breakpoint row,  will be deleted
-        self.limit_orders.append((self.time + 1, -1.0, 1, 0)) #breakpoint row,  will be deleted
-        
+        self.market_orders.loc[len(self.market_orders)] = [self.time + 1, -1.0, 1]    #breakpoint row,  will be deleted
+        self.cancelations.loc[len(self.cancelations)] = [self.time + 1, -1.0, 1, 0]   #breakpoint row,  will be deleted
+        self.limit_orders.loc[len(self.limit_orders)] = [self.time + 1, -1.0, 1, 0]   #breakpoint row,  will be deleted
+       
         
         order_books = []
-        order_books.append(zip(0, self.OB_0))
-        while (market_index != len_m & cncl_index != len_c & limit_index != len_l ):
-            if ( self.market_orders[market_index][0] <= self.cancelations[cncl_index][0] 
-            & self.market_orders[limit_index][0] <= self.limit_orders[limit_index][0] ):
+        order_books.append((0, self.OB_0))
+        
+        while ((market_index != len_m) & (cncl_index != len_c) & (limit_index != len_l)):
+            if ((self.market_orders.t[market_index] <= self.cancelations.t[cncl_index]) 
+            & (self.market_orders.t[market_index] <= self.limit_orders.t[limit_index] )):
                 
-                cur_ob = self.OB(self.market_orders[market_index][0], order_books[-1][1].bids, order_books[-1][1].asks)
-                self.market_orders_report.append(cur_ob.process_market_order(self.market_orders[market_index]))
-                order_books.append(zip(self.market_orders[market_index][0], cur_ob))
+                cur_ob = self.OB(self.market_orders.t[market_index], order_books[-1][1].bids, order_books[-1][1].asks)
+                self.market_orders_report.loc[len(self.market_orders_report)] = cur_ob.process_market_order(self.market_orders.loc[market_index])
+                order_books.append((self.market_orders.t[market_index], cur_ob))
                 market_index += 1
                 
-            elif( self.cancelations[cncl_index][0] <=  self.market_orders[market_index][0]
-            & self.cancelations[cncl_index][0] <= self.limit_orders[limit_index][0] ):
-                cur_ob = self.OB(self.cancelations[cncl_index][0], order_books[-1][1].bids, order_books[-1][1].asks)
-                cur_ob.process_cancelation(self.cancelations[cncl_index])
+            elif ((self.cancelations.t[cncl_index] <=  self.market_orders.t[market_index])
+            & (self.cancelations.t[cncl_index] <= self.limit_orders.t[limit_index])):
+                
+                cur_ob = self.OB(self.cancelations.t[cncl_index], order_books[-1][1].bids, order_books[-1][1].asks)
+                cur_ob.process_cancelation(self.cancelations.loc[cncl_index])
+                order_books.append((self.cancelations.t[cncl_index], cur_ob))
+                cncl_index += 1
+                
+            elif ((self.limit_orders.t[limit_index] <= self.cancelations.t[cncl_index])
+            & (self.limit_orders.t[limit_index] <= self.market_orders.t[market_index])):
+                
+                cur_ob = self.OB(self.limit_orders.t[limit_index], order_books[-1][1].bids, order_books[-1][1].asks)
+                cur_ob.process_limit_order(self.limit_orders.loc[limit_index])
+                order_books.append((self.limit_orders.t[limit_index], cur_ob))
+                limit_index += 1
+                
+        self.market_orders.drop(index = self.market_orders.index[-1], axis=0, inplace=True) 
+        self.cancelations.drop(index = self.cancelations.index[-1], axis=0, inplace=True) 
+        self.limit_orders.drop(index = self.limit_orders.index[-1], axis=0, inplace=True) 
+        
+        return order_books
                 
 time = 100
 lambda_0 = 0.25
@@ -248,6 +330,7 @@ lambda_0_limit = 5
 lambda_0_cncl = 1
 
 sim = Simulator(time, lambda_0, a, var_e, q_0, var_q, b, start_mid, depth, step, density, lambda_0_limit, lambda_0_cncl)
-print(sim.OB_0.bids)
-m = sim.OB_0.process_limit_order((0.11119819966925046, 1, 0.6831, 39))
-print(sim.OB_0.bids)
+#print(sim.cancelations.loc[5])
+
+#sim.OB_0.process_cancelation(sim.cancelations.loc[5])
+#print(sim.OB_O.asks.loc[5])
